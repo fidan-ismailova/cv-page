890af1e02cc06d9af092b397b54caaaa86fc05f3
Fidan Ismayilova <fidan_ismailova@git.foxminded.com.ua>
2021-10-14 15:18:08 +0400

    change experience

--- build/js/experience.js
+++ build/js/experience.js
@@ -1,18 +1,26 @@
 const experience = [
     {
-        "company": "Object",
-        "city": "Array",
-        "from": "NaN",
-        "to": 'Infinity',
-        "position": "undefined",
-        "description": "null"
+        "company": "COMPANY NAME 3",
+        "city": "Tayler",
+        "from": "2016",
+        "to": 'Present',
+        "position": "your position",
+        "description": "Replace with your key responsibilities, accomplishments and skills that may relevant to the position you are applying for Include the keywords from the listing to capture the employer’s attention and to match applicant tracking system requirements Transform your responsibilities into quantifiable achievements by brainstorming for any numbers relevant to your key duties"
     },
     {
-        "company": "Company_Name",
-        "city": "City",
-        "from": "2010",
+        "company": "COMPANY NAME",
+        "city": "NEW YORK",
+        "from": "2016",
+        "to": 'Present',
+        "position": "your position",
+        "description": "Replace with your key responsibilities, accomplishments and skills that may relevant to the position you are applying for Include the keywords from the listing to capture the employer’s attention and to match applicant tracking system requirements Transform your responsibilities into quantifiable achievements by brainstorming for any numbers relevant to your key duties"
+    },
+    {
+        "company": "COMPANY NAME 2",
+        "city": "London",
+        "from": "2013",
         "to": '2014',
-        "position": "position",
-        "description": "description"
+        "position": "your position",
+        "description": "Replace with your key responsibilities, accomplishments and skills that may relevant to the position you are applying for Include the keywords from the listing to capture the employer’s attention and to match applicant tracking system requirements Transform your responsibilities into quantifiable achievements by brainstorming for any numbers relevant to your key duties"
     }
 ]
